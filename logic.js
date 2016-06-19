373e6a4ed072f009f18c88144c7dd6f0a69a4a32
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-19 00:39:33 -0400

    added form reset function

--- logic.js
+++ logic.js
@@ -1,11 +1,33 @@
+//VARIABLES
 var dataRef = new Firebase("hyruletrak.firebaseIO.com");
 var name;
 var destination;
 var firstTrain;
 var frequency;
 
+//FUNCTIONS
+
 
 $(document).ready(function() {
+//Firebase watcher + initial loader HINT: .on("value")
+dataRef.on("value", function(snapshot) {
+
+  var train = snapshot.val();
+  // Log everything that's coming out of snapshot
+  console.log(train);
+
+  // Change the HTML to reflect
+  $("#namedisplay").html(snapshot.val().name);
+  $("#emaildisplay").html(snapshot.val().email);
+  $("#agedisplay").html(snapshot.val().age);
+  $("#commentdisplay").html(snapshot.val().comment);
+
+
+// Handle the errors
+}, function(errorObject){
+
+  console.log("Errors handled: " + errorObject.code)
+});
 
 $("#submit-btn").on("click", function(){
      name = $('#nameInput').val();
@@ -21,9 +43,12 @@ $("#submit-btn").on("click", function(){
        frequency: frequency,
    });
  
+  document.getElementById("train").reset();
+  
    return false;
 });
 
+/*
 dataRef.on("child_added", function(childSnapshot) {
 
     displayData(childSnapshot);
@@ -37,21 +62,21 @@ dataRef.orderByChild("dateAdded").on("child_added", function(snapshot){
     $("#firstTrainDisplay").html(snapshot.firstTrain.val());
     $("#frequencyDisplay").html(snapshot.frequency.val());
   });
-/*
+
     //full list of items to the well (well is from recentUser app)
 
     $('#full-member-list').append(
         "<div class='well'><span id='name'> " + childSnapshot.val().name+" </span><span id="role"
         + childSnapshot.val().startDate+" </span><span id="role"
-*/
+
 
 
 function displayData(childSnapshot) {
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().firstTrain);
-    console.log(childSnapshot.val().frequency);
+    console.log(childSnapshot.name.val);
+    console.log(childSnapshot.destination.val);
+    console.log(childSnapshot.firstTrain.val);
+    console.log(childSnapshot.frequency.val);
     
 }
-
+*/
 });
