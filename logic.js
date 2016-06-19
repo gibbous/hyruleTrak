ad9b0c8512bf7e29250d118f2a0a1e51f2421ae9
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-18 23:38:15 -0400

    posting to firebase is working. retrieving from firebase is not

--- logic.js
+++ logic.js
@@ -10,16 +10,17 @@ $(document).ready(function() {
 $("#submit-btn").on("click", function(){
      name = $('#nameInput').val();
     destination = $('#destinationInput').val();
-    startDate = $('#firstTrainInput').val();
-    monthlyRate = $('#frequencyInput').val();
+    firstTrain = $('#firstTrainInput').val();
+    frequency = $('#frequencyInput').val();
+
 
     dataRef.push({
        name: name,
        destination: destination,
        firstTrain: firstTrain,
        frequency: frequency,
-   })
-
+   });
+ 
    return false;
 });
 
@@ -27,22 +28,23 @@ dataRef.on("child_added", function(childSnapshot) {
 
     displayData(childSnapshot);
 });
-/*
 
-dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-    // Change the HTML to reflect
+
+dataRef.orderByChild("dateAdded").on("child_added", function(snapshot){
+    
     $("#nameDisplay").html(snapshot.name.val());
-    $("#roleDisplay").html(snapshot.name.val());
-    $("#startDateDisplay").html(snapshot.name.val());
-    $("#monthlyRateDisplay").html(snapshot.name.val());
-
+    $("#destinationDisplay").html(snapshot.destination.val());
+    $("#firstTrainDisplay").html(snapshot.firstTrain.val());
+    $("#frequencyDisplay").html(snapshot.frequency.val());
+  });
+/*
     //full list of items to the well (well is from recentUser app)
 
     $('#full-member-list').append(
         "<div class='well'><span id='name'> " + childSnapshot.val().name+" </span><span id="role"
         + childSnapshot.val().startDate+" </span><span id="role"
-
 */
+
 
 function displayData(childSnapshot) {
     console.log(childSnapshot.val().name);
