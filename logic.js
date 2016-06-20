856c3f0d69d29255237518a954ecb680e80f50da
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-19 22:19:11 -0400

    adding train schedule to train timetable

--- logic.js
+++ logic.js
@@ -1,5 +1,8 @@
 //VARIABLES
+
+//Link to Firebase
 var dataRef = new Firebase("hyruletrak.firebaseIO.com");
+
 var name;
 var destination;
 var firstTrain;
@@ -16,14 +19,16 @@ function displayData(childSnapshot) {
 
 
 $(document).ready(function() {
-//Firebase watcher + initial loader HINT: .on("value")
-dataRef.on("value", function(childSnapshot) {
+//Firebase watcher + initial loader 
+dataRef.on("value", function(snapshot) {
+
+snapshot.forEach(function(snapshot) {
+  $("#nameDisplay").html(snapshot.val().name);
+  $("#destinationDisplay").html(snapshot.val().destination);
+  $("#frequencyDisplay").html(snapshot.val().frequency);
+  });
 
 
-  $("#nameDisplay").html(childSnapshot.val().name);
-  $("#destinationDisplay").html(childSnapshot.val().destination);
-  $("#frequencyDisplay").html(childSnapshot.val().frequency);
-  
 
 
 // Handle the errors
@@ -32,13 +37,14 @@ dataRef.on("value", function(childSnapshot) {
   console.log("Errors handled: " + errorObject.code)
 });
 
+//Grab user input
 $("#submit-btn").on("click", function(){
-     name = $('#nameInput').val();
+    name = $('#nameInput').val();
     destination = $('#destinationInput').val();
     firstTrain = $('#firstTrainInput').val();
     frequency = $('#frequencyInput').val();
 
-
+//Add new train to Firebase
     dataRef.push({
        name: name,
        destination: destination,
@@ -46,21 +52,16 @@ $("#submit-btn").on("click", function(){
        frequency: frequency,
    });
  
+ //Reset form fields
   document.getElementById("train").reset();
-  
+ 
+ //Prevent page from reloading 
    return false;
 });
 
-dataRef.on("child_added", function(childSnapshot) {
 
-    displayData(childSnapshot);
-   
-});
-
-
-
-
-dataRef.on("child_added", function(childSnapshot) {
+//Add new train to timetable
+dataRef.on("child_added", function(childSnapshot, prevChildKey) {
 
     displayData(childSnapshot);
 });
@@ -68,10 +69,32 @@ dataRef.on("child_added", function(childSnapshot) {
 
 dataRef.orderByChild("dateAdded").on("child_added", function(snapshot){
     
-    $("#nameDisplay").html(snapshot.val().name);
-    $("#destinationDisplay").html(snapshot.val().destination);
-    $("#firstTrainDisplay").html(snapshot.val().firstTrain);
-    $("#frequencyDisplay").html(snapshot.val().frequency);
+    var trainName = snapshot.val().name;
+    var trainDestination =snapshot.val().destination;
+    var trainStart = snapshot.val().firstTrain;
+    var trainFreq = snapshot.val().frequency;
+
+// Add each train's data into the table
+  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainStart + "</td><td>" + trainFreq + "</td><td>" + "placeholder" + "</td><td>" + "placeholder2" + "</td></tr>");
+
+  var tableRow = $("<tr>");
+  var tableData1 = $("<td>");
+  tableData1.html(trainName);
+  var tableData2 = $("<td>");
+  tableData2.html(trainDestination);
+  var tableData3 = $("<td>");
+  var tableData4 = $("<td>");
+  tableRow.append(tableData1);
+  tableRow.append(tableData2);
+  tableRow.append(tableData3);
+  tableRow.append(tableData4);
+  $("#trainTable > tbody").append(tableRow);
+
+
+
+
+
+
   });
 
     
