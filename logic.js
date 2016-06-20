9fa32b94cd4f19d9729ca99410084ab8466ec346
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-06-19 22:47:26 -0400

    removed extra rows

--- logic.js
+++ logic.js
@@ -19,23 +19,6 @@ function displayData(childSnapshot) {
 
 
 $(document).ready(function() {
-//Firebase watcher + initial loader 
-dataRef.on("value", function(snapshot) {
-
-snapshot.forEach(function(snapshot) {
-  $("#nameDisplay").html(snapshot.val().name);
-  $("#destinationDisplay").html(snapshot.val().destination);
-  $("#frequencyDisplay").html(snapshot.val().frequency);
-  });
-
-
-
-
-// Handle the errors
-}, function(errorObject){
-
-  console.log("Errors handled: " + errorObject.code)
-});
 
 //Grab user input
 $("#submit-btn").on("click", function(){
@@ -60,12 +43,7 @@ $("#submit-btn").on("click", function(){
 });
 
 
-//Add new train to timetable
-dataRef.on("child_added", function(childSnapshot, prevChildKey) {
-
-    displayData(childSnapshot);
-});
-
+//Load train information from firebase to timetable
 
 dataRef.orderByChild("dateAdded").on("child_added", function(snapshot){
     
@@ -75,25 +53,7 @@ dataRef.orderByChild("dateAdded").on("child_added", function(snapshot){
     var trainFreq = snapshot.val().frequency;
 
 // Add each train's data into the table
-  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainStart + "</td><td>" + trainFreq + "</td><td>" + "placeholder" + "</td><td>" + "placeholder2" + "</td></tr>");
-
-  var tableRow = $("<tr>");
-  var tableData1 = $("<td>");
-  tableData1.html(trainName);
-  var tableData2 = $("<td>");
-  tableData2.html(trainDestination);
-  var tableData3 = $("<td>");
-  var tableData4 = $("<td>");
-  tableRow.append(tableData1);
-  tableRow.append(tableData2);
-  tableRow.append(tableData3);
-  tableRow.append(tableData4);
-  $("#trainTable > tbody").append(tableRow);
-
-
-
-
-
+  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFreq + "</td><td>" + "nextArrival" + "</td><td>" + "minutesAway" + "</td></tr>");
 
   });
 
